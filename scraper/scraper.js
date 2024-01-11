ed73d03accb435bb9d31b40eb9eb8ab60f9c750f
Faheem798 <faheemrana798@gmail.com>
2024-01-11 09:30:11 +0500

    �nothing to say

--- scraper/scraper.js
+++ scraper/scraper.js
@@ -15,35 +15,34 @@ export async function scrapeData(url) {
 
   const priceElement = await page.waitForXPath('//div[@class="QORQHb"]//span');
   const price = await page.evaluate(el => el.textContent.trim(), priceElement);
-
+  const departureTimeElements = await page.$$eval('div.c257Jb.QwxBBf.NbfThf > div.OJg28c.YMlIz.y52p7d',
+    elements => elements.map(el => el.textContent.replace(/\+1$/, "").trim()));
   const departingFlight = {
     originCode: await page.waitForXPath('//div[contains(@class, "ZHa2lc tdMWuf y52p7d")]//span[contains(@dir, "ltr") and normalize-space()]')
       .then(element => page.evaluate(el => el.textContent.trim(), element)),
     departureDate: await page.waitForSelector('.tMLpW')
       .then(element => page.evaluate(el => el.textContent.trim(), element)),
-    departureTime: await page.waitForXPath('//div[contains(@class, "b0EVyb YMlIz y52p7d")][normalize-space()]')
-      .then(element => page.evaluate(el => el.textContent.trim(), element)),
+    departureTime: await page.waitForXPath('//div[contains(@class, "b0EVyb YMlIz y52p7d")][normalize-space()] | //div[@class="b0EVyb YMlIz y52p7d"][normalize-space()] ')
+      .then(element => page.evaluate(el => el.textContent.replace(/\+1$/, "").trim(), element)),
     destinationCode: await page.waitForXPath('//div[contains(@class, "FY5t7d tdMWuf y52p7d")]//span[contains(@dir, "ltr") and normalize-space()]')
       .then(element => page.evaluate(el => el.textContent.trim(), element)),
     arrivalDate: await page.waitForSelector('.tMLpW')
       .then(element => page.evaluate(el => el.textContent.trim(), element)),
-    arrivalTime: await page.waitForSelector('#c26 > div.c257Jb.QwxBBf.NbfThf > div.OJg28c.YMlIz.y52p7d')
-      .then(element => page.evaluate(el => el.textContent.trim(), element)),
+    arrivalTime: departureTimeElements[0],
   };
 
   const returningFlight = {
-    originCode: await page.waitForXPath('//div[contains(@class, "ZHa2lc tdMWuf y52p7d")]//span[contains(@dir, "ltr") and normalize-space()]')
+    originCode: await page.waitForXPath('(//div[contains(@class, "ZHa2lc tdMWuf y52p7d")]//span[contains(@dir, "ltr") and normalize-space()])[2]')
       .then(element => page.evaluate(el => el.textContent.trim(), element)),
     departureDate: await page.waitForSelector('div:nth-child(2) > div.uQEzCd > div > div > div.tMLpW')
       .then(element => page.evaluate(el => el.textContent.trim(), element)),
-    departureTime: await page.waitForSelector('#c35 > div.c257Jb.QwxBBf.NbfThf > div.b0EVyb.YMlIz.y52p7d')
-      .then(element => page.evaluate(el => el.textContent.trim(), element)),
-    destinationCode: await page.waitForXPath('//div[contains(@class, "FY5t7d tdMWuf y52p7d")]//span[contains(@dir, "ltr") and normalize-space()]')
+    departureTime: await page.waitForSelector('#c35 > div.c257Jb.QwxBBf.NbfThf > div.b0EVyb.YMlIz.y52p7d, #c41 > div.c257Jb.QwxBBf.NbfThf > div.b0EVyb.YMlIz.y52p7d, #c51 > div.c257Jb.QwxBBf.NbfThf > div.b0EVyb.YMlIz.y52p7d, #c53 > div.c257Jb.QwxBBf.NbfThf > div.b0EVyb.YMlIz.y52p7d')
+      .then(element => page.evaluate(el => el.textContent.replace(/\+1$/, "").trim(), element)),
+    destinationCode: await page.waitForXPath('(//div[contains(@class, "FY5t7d tdMWuf y52p7d")]//span[contains(@dir, "ltr") and normalize-space()])[2]')
       .then(element => page.evaluate(el => el.textContent.trim(), element)),
     arrivalDate: await page.waitForSelector('div:nth-child(2) > div.uQEzCd > div > div > div.tMLpW')
       .then(element => page.evaluate(el => el.textContent.trim(), element)),
-    arrivalTime: await page.waitForSelector('#c35 > div.c257Jb.QwxBBf.NbfThf > div.OJg28c.YMlIz.y52p7d')
-      .then(element => page.evaluate(el => el.textContent.trim(), element)),
+    arrivalTime: departureTimeElements[1]
   };
 
   await browser.close();
--- utils/data.js
+++ utils/data.js
@@ -1,15 +1,13 @@
 export const urls = [
-  "https://www.google.com/travel/flights/s/sQbK4YSNwJgUkKFA8",
-  "https://www.google.com/travel/flights/s/WEf2XB4KVHhjvsG56",
+  'https://www.google.com/travel/flights/booking?tfs=CBwQAhpgEgoyMDI0LTAyLTEyIh4KA0pGSxIKMjAyNC0wMi0xMhoDR1ZBKgJMWDICMjMiIAoDR1ZBEgoyMDI0LTAyLTEzGgNBVEgqAkxYMgQxODIyagcIARIDSkZLcgcIARIDQVRIGl4SCjIwMjQtMDItMTQiHwoDQVRIEgoyMDI0LTAyLTE0GgNUTFYqAkxZMgM1NDQiHQoDVExWEgoyMDI0LTAyLTE1GgNKRksqAkxZMgE5agcIARIDQVRIcgcIARIDSkZLQAFIAXABggELCP___________wGYAQE&tfu=CnBDalJJZDFORGVHOTNaa05rTFRoQlF6UnNaWGRDUnkwdExTMHRMUzB0TFMxM1pXSXhOVUZCUVVGQlIxZG1ZbEJ2UjJSVmMzRkJFZ2xNV1RVME5IeE1XVGthQ3dpaXExY1FBQm9EVUV0U09CeHd5cE1mEgIIASIA',
+  // "https://www.google.com/travel/flights/s/w2HucPV64nFtGyqcA",
+  // "https://www.google.com/travel/flights/s/cQKwE5aVXkBtMCEZ8",
+  // "https://www.google.com/travel/flights/s/j9orXhWCoS7N6Crg7",
+  // "https://www.google.com/travel/flights/s/bBTMbPmWhr8E4jzQA",
+  // "https://www.google.com/travel/flights/s/N1fRq4i2LrGHjZ648",
+  // "https://www.google.com/travel/flights/s/6wL69SctfECo2hCPA",
+  // "https://www.google.com/travel/flights/s/PevAfDUv1G5aACKJ7",
+  // "https://www.google.com/travel/flights/s/WmqsfrtGrTAR7eHN9",
+  // "https://www.google.com/travel/flights/s/sQbK4YSNwJgUkKFA8",
+  // "https://www.google.com/travel/flights/s/WEf2XB4KVHhjvsG56",
 ];
-
-const multiUrls =[
-  "https://www.google.com/travel/flights/s/w2HucPV64nFtGyqcA",
-  "https://www.google.com/travel/flights/s/cQKwE5aVXkBtMCEZ8",
-  "https://www.google.com/travel/flights/s/j9orXhWCoS7N6Crg7",
-  "https://www.google.com/travel/flights/s/bBTMbPmWhr8E4jzQA",
-  "https://www.google.com/travel/flights/s/N1fRq4i2LrGHjZ648",
-  "https://www.google.com/travel/flights/s/6wL69SctfECo2hCPA",
-  "https://www.google.com/travel/flights/s/PevAfDUv1G5aACKJ7",
-  "https://www.google.com/travel/flights/s/WmqsfrtGrTAR7eHN9",
-]
