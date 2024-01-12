44fa2b034dc89defb1e0116b9922779984aeabe4
Faheem798 <faheemrana798@gmail.com>
2024-01-12 09:43:22 +0500

    Updated version

--- scraper/scraper.js
+++ scraper/scraper.js
@@ -32,13 +32,13 @@ export async function scrapeData(url) {
   };
 
   const returningFlight = {
-    originCode: await page.waitForXPath('(//div[contains(@class, "ZHa2lc tdMWuf y52p7d")]//span[contains(@dir, "ltr") and normalize-space()])[2]')
+    originCode: await page.waitForSelector('div:nth-child(2) > div.mz0jqb > div > .yJwmMb > div.c257Jb.QwxBBf.NbfThf > div.ZHa2lc.tdMWuf.y52p7d > span')
       .then(element => page.evaluate(el => el.textContent.trim(), element)),
     departureDate: await page.waitForSelector('div:nth-child(2) > div.uQEzCd > div > div > div.tMLpW')
       .then(element => page.evaluate(el => el.textContent.trim(), element)),
-    departureTime: await page.waitForSelector('#c35 > div.c257Jb.QwxBBf.NbfThf > div.b0EVyb.YMlIz.y52p7d, #c41 > div.c257Jb.QwxBBf.NbfThf > div.b0EVyb.YMlIz.y52p7d, #c51 > div.c257Jb.QwxBBf.NbfThf > div.b0EVyb.YMlIz.y52p7d, #c53 > div.c257Jb.QwxBBf.NbfThf > div.b0EVyb.YMlIz.y52p7d')
+    departureTime: await page.waitForSelector('div:nth-child(2) > div.mz0jqb > div > .yJwmMb > div.c257Jb.QwxBBf.NbfThf > div.b0EVyb.YMlIz.y52p7d')
       .then(element => page.evaluate(el => el.textContent.replace(/\+1$/, "").trim(), element)),
-    destinationCode: await page.waitForXPath('(//div[contains(@class, "FY5t7d tdMWuf y52p7d")]//span[contains(@dir, "ltr") and normalize-space()])[2]')
+    destinationCode: await page.waitForSelector('div:nth-child(2) > div.mz0jqb > div > .yJwmMb > div.c257Jb.QwxBBf.NbfThf > div.FY5t7d.tdMWuf.y52p7d > span')
       .then(element => page.evaluate(el => el.textContent.trim(), element)),
     arrivalDate: await page.waitForSelector('div:nth-child(2) > div.uQEzCd > div > div > div.tMLpW')
       .then(element => page.evaluate(el => el.textContent.trim(), element)),
--- utils/data.js
+++ utils/data.js
@@ -1,8 +1,8 @@
 export const urls = [
   'https://www.google.com/travel/flights/booking?tfs=CBwQAhpgEgoyMDI0LTAyLTEyIh4KA0pGSxIKMjAyNC0wMi0xMhoDR1ZBKgJMWDICMjMiIAoDR1ZBEgoyMDI0LTAyLTEzGgNBVEgqAkxYMgQxODIyagcIARIDSkZLcgcIARIDQVRIGl4SCjIwMjQtMDItMTQiHwoDQVRIEgoyMDI0LTAyLTE0GgNUTFYqAkxZMgM1NDQiHQoDVExWEgoyMDI0LTAyLTE1GgNKRksqAkxZMgE5agcIARIDQVRIcgcIARIDSkZLQAFIAXABggELCP___________wGYAQE&tfu=CnBDalJJZDFORGVHOTNaa05rTFRoQlF6UnNaWGRDUnkwdExTMHRMUzB0TFMxM1pXSXhOVUZCUVVGQlIxZG1ZbEJ2UjJSVmMzRkJFZ2xNV1RVME5IeE1XVGthQ3dpaXExY1FBQm9EVUV0U09CeHd5cE1mEgIIASIA',
-  // "https://www.google.com/travel/flights/s/w2HucPV64nFtGyqcA",
-  // "https://www.google.com/travel/flights/s/cQKwE5aVXkBtMCEZ8",
-  // "https://www.google.com/travel/flights/s/j9orXhWCoS7N6Crg7",
+  "https://www.google.com/travel/flights/s/w2HucPV64nFtGyqcA",
+  "https://www.google.com/travel/flights/s/cQKwE5aVXkBtMCEZ8",
+  "https://www.google.com/travel/flights/s/j9orXhWCoS7N6Crg7",
   // "https://www.google.com/travel/flights/s/bBTMbPmWhr8E4jzQA",
   // "https://www.google.com/travel/flights/s/N1fRq4i2LrGHjZ648",
   // "https://www.google.com/travel/flights/s/6wL69SctfECo2hCPA",
